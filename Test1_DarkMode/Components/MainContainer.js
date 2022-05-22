4dff944facd5ba66a7092f7489d0f75ed4438d5e
lavpalve <53850659+lavpalve@users.noreply.github.com>
2022-05-22 19:12:05 +0530

    Create MainContainer.js

---
+++ Test1_DarkMode/Components/MainContainer.js
@@ -0,0 +1,9 @@
+function MainContainer(){
+  return(
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <span>This is white theme</span>
+        </div>
+    </div>
+  )
+}
