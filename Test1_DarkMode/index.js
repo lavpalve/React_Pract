105be220dec0c018946c78e66e2879d9fe14412f
lavpalve <53850659+lavpalve@users.noreply.github.com>
2022-05-22 20:09:21 +0530

    Update index.js

--- Test1_DarkMode/index.js
+++ Test1_DarkMode/index.js
@@ -2,7 +2,7 @@ var root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(<App />);
             
             
-document.getElementById('btn').addEventListener('click', ()=>{
+function changeStyle(){
     document.getElementById('mainCont').style.backgroundColor = '#0f0f0f';
     document.getElementById('text').innerText = 'This is dark theme';
     document.getElementById('text').style.color = 'f0f0f0';
