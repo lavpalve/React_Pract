8247ca7639504901677d2573b3a41994effa0b2a
lavpalve <53850659+lavpalve@users.noreply.github.com>
2022-05-22 19:20:23 +0530

    Update MainContainer.js

--- Test1_DarkMode/Components/MainContainer.js
+++ Test1_DarkMode/Components/MainContainer.js
@@ -1,8 +1,8 @@
 function MainContainer(){
   return(
     <div class="row justify-content-center">
-        <div class="col-8">
-            <span>This is white theme</span>
+        <div class="col-8 d-flex justify-content-center" style="padding: auto">
+            <h2>This is white theme</h2>
         </div>
     </div>
   )
