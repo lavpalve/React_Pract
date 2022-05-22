688bcc66e5e04030452ec7153b297e678c134668
lavpalve <53850659+lavpalve@users.noreply.github.com>
2022-05-22 20:20:10 +0530

    Update ButtonComponent.js

--- Test1_DarkMode/Components/ButtonComponent.js
+++ Test1_DarkMode/Components/ButtonComponent.js
@@ -1,5 +1,5 @@
 function ButtonComponent(){
   return(
-    <button id="btn" class="btn btn-primary" onclick="changeStyle()">Click Me</button>
+    <button id="btn" class="btn btn-primary" onClick="changeStyle">Click Me</button>
   )
 }
